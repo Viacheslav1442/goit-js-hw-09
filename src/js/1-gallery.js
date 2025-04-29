a718290ba18214c61b1b64f17f17b46f7e5a2359
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-04-29 10:58:16 +0300

    js

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -19,3 +19,6 @@ new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,
 });
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
