4a4441be82f4da8870ff51fac896974abdb4ad50
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-04-29 16:53:22 +0000

    Deploying to gh-pages from @ Viacheslav1442/goit-js-hw-09@715940b74c122fc51806cc4980a808930018888c 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,7 +1,12 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as t}from"./assets/vendor-D0gBiHs0.js";const i=[{preview:"https://placehold.co/150x100?text=Preview+1",original:"https://placehold.co/800x600?text=Original+1",description:"Example Image 1"},{preview:"https://placehold.co/150x100?text=Preview+2",original:"https://placehold.co/800x600?text=Original+2",description:"Example Image 2"},{preview:"https://placehold.co/150x100?text=Preview+3",original:"https://placehold.co/800x600?text=Original+3",description:"Example Image 3"}],r=document.querySelector(".gallery"),o=i.map(({preview:e,original:l,description:a})=>`
-    <li class="gallery-item">
-      <a class="gallery-link" href="${l}">
-        <img class="gallery-image" src="${e}" alt="${a}" />
-      </a>
-    </li>`).join("");r.innerHTML=o;new t(".gallery a",{captionsData:"alt",captionDelay:250});
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as a}from"./assets/vendor-D0gBiHs0.js";const i=[{preview:"https://placehold.co/600x400?text=Thumb1",original:"https://placehold.co/1200x800?text=Image1",description:"Image 1 description"},{preview:"https://placehold.co/600x400?text=Thumb2",original:"https://placehold.co/1200x800?text=Image2",description:"Image 2 description"}],o=document.querySelector(".gallery"),r=i.map(({preview:e,original:l,description:t})=>`
+  <li class="gallery-item">
+    <a class="gallery-link" href="${l}">
+      <img
+        class="gallery-image"
+        src="${e}"
+        alt="${t}"
+      />
+    </a>
+  </li>
+`).join("");o.innerHTML=r;new a(".gallery a",{captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=1-gallery.js.map
